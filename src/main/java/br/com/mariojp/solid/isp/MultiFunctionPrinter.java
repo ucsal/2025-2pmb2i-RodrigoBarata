831acf6ab27b5086654fdb43c626148f8c541f5b
Your Name <you@example.com>
2025-09-03 22:04:18 -0300

    Refatoração do serviço de impressão para utilizar a interface Printer, removendo a dependência de funcionalidades desnecessárias como o scan. O código agora está mais alinhado com o princípio da segregação de interfaces.

---
+++ src/main/java/br/com/mariojp/solid/isp/MultiFunctionPrinter.java
@@ -0,0 +1,13 @@
+package br.com.mariojp.solid.isp;
+
+public class MultiFunctionPrinter implements Printer, Scanner {
+    @Override 
+    public void print(String content) { 
+        /* imprime */ 
+    }
+    
+    @Override 
+    public void scan(String targetFile) { 
+        /* escaneia */ 
+    }
+}
